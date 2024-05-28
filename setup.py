81b3bb6761081aff22c56bd861885c7ecc01dc9b
dkenward <kenward.dusten@gmail.com>
2024-05-28 07:55:11 -0600

    Readded game.py and setup to enable pydun from command line

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ setup(
     version='0.0.1',
     packages=find_packages("src"),
     package_dir={"":"src"},
-    # entry_points = {
-    #     'console_scripts': [
-    #         'pydun = game:main',
-    #     ]
-    # },
+    entry_points = {
+        'console_scripts': [
+            'pydun = game:main',
+        ]
+    },
     long_description='Simple Dungeon Crawler in Python',
     url='https://github.com/No-More-Discussion-Posts/PyDun-Crawler',
     author='The Team'
