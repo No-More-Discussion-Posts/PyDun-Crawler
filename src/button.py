9f7c63fb113d005a8b13a953cc9fb70ec252eb59
Trethris <barrtyler@outlook.com>
2024-05-29 21:48:32 -0400

    Cleaned up the button class so that all you have to do is input the name, game, x and y and itll autobuild it. Then you just gotta do buttonname.show

--- src/button.py
+++ src/button.py
@@ -1,10 +1,14 @@
 import pygame
 
 class button:
-    def __init__(self, name, x, y):
+    def __init__(self, name, game, x, y):
         self.DEFAULT_FONT = pygame.font.get_default_font()
         FONT = pygame.font.Font(self.DEFAULT_FONT,20)
+        self.game = game
         self.button_surface = pygame.Surface((135, 60))
         self.text = FONT.render(name, True, (255, 255, 255))
         self.rect = self.text.get_rect(center=(self.button_surface.get_width()/2, self.button_surface.get_height()/2))
         self.butt_rect = pygame.Rect(x, y, 135, 60)
+        self.blit = self.button_surface.blit(self.text, self.rect)
+        self.show = self.game.screen.blit(self.button_surface, (self.butt_rect.x, self.butt_rect.y))
+
