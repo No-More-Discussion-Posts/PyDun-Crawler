87ea685027b41e99874f705cf981ce555d6c736f
dkenward <kenward.dusten@gmail.com>
2024-05-31 11:16:30 -0600

    Tests included

--- setup.py
+++ setup.py
@@ -1,18 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-<<<<<<< Updated upstream
     name="copd",
     version="0.0.1",
     packages=['copd'],
     entry_points={"console_scripts": ["copd = game:main"]},
-=======
-    name="pydun-crawler",
-    version="0.0.1",
-    packages=find_packages('src'),
-    package_dir={'':'src'},
-    entry_points={"console_scripts": ["pydun = game:main"]},
->>>>>>> Stashed changes
     long_description="Simple Dungeon Crawler in Python",
     url="https://github.com/No-More-Discussion-Posts/PyDun-Crawler",
     author="The Team",
