f671c0f5977131a44454b9bc805b8c9bd1c4afd6
dkenward <kenward.dusten@gmail.com>
2024-05-31 11:31:24 -0600

    Fixed setup.py error

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="copd",
     version="0.0.1",
     packages=['copd'],
-    entry_points={"console_scripts": ["copd = game:main"]},
+    entry_points={"console_scripts": ["copd = copd.game:main"]},
     long_description="Simple Dungeon Crawler in Python",
     url="https://github.com/No-More-Discussion-Posts/PyDun-Crawler",
     author="The Team",
--- src/player.py
+++
@@ -1,5 +0,0 @@
-class Player:
-    def __init__(self, hp, defense, power):
-        self.hp = hp
-        self.defense = defense
-        self.power = power
