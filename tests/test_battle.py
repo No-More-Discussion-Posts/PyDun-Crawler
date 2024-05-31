57091fd75553c46ca68eece03c5366d646b2f4b7
dkenward <kenward.dusten@gmail.com>
2024-05-31 12:02:09 -0600

    added blank tests

---
+++ tests/test_battle.py
@@ -0,0 +1,17 @@
+import pytest
+
+def test_fight(game):
+    pass
+
+def test_defend(game):
+    pass
+
+def test_run(game):
+    pass
+
+def test_player_death(game):
+    pass
+
+def test_monster_death(game):
+    pass
+
