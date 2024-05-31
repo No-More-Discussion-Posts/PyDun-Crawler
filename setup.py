b4123d7267e7854f58ceb73dcd28818d952160ba
dkenward <kenward.dusten@gmail.com>
2024-05-31 10:21:46 -0600

    refactor src to copd

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="copd",
     version="0.0.1",
-    packages=['src'],
+    packages=['copd'],
     entry_points={"console_scripts": ["copd = game:main"]},
     long_description="Simple Dungeon Crawler in Python",
     url="https://github.com/No-More-Discussion-Posts/PyDun-Crawler",
