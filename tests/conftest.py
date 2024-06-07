8753b135efd34a77ad84d19af9d993a70ced8243
dkenward <kenward.dusten@gmail.com>
2024-06-06 21:50:24 -0600

    Fixed conftest.py for github workflow

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,13 @@
 import pytest
-from copd.engine import Engine
-from copd.config import *
 import pygame
 import os
+import sys
+if 'copd' not in sys.modules:
+    parent_dir_name = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    sys.path.append(parent_dir_name)
+from copd.engine import Engine
+from copd.config import *
+
 os.environ["SDL_VIDEODRIVER"] = "dummy"
 pygame.init()
 
