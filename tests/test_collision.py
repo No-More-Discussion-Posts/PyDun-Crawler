2639d38a240e1ce4f1592921cb19f06656a92ce8
dkenward <kenward.dusten@gmail.com>
2024-06-08 13:55:40 -0600

    Added e/w wall collision

--- tests/test_collision.py
+++ tests/test_collision.py
@@ -31,3 +31,28 @@ def test_collide_wall_s(game):
     y = player.get(Position).y
     print(player.get(Position))
     assert (x,y) == (15,16)
+
+def test_collide_wall_e(game):
+    player = game.player #should be brand new
+    movement = pygame.event.Event(pygame.KEYDOWN,dict(key=pygame.K_w))
+    game.handle_event(movement)
+    for i in range(20):
+        movement = pygame.event.Event(pygame.KEYDOWN,dict(key=pygame.K_d))
+        game.handle_event(movement)
+    x = player.get(Position).x
+    y = player.get(Position).y
+    print(player.get(Position))
+    assert (x,y) == (30,8)
+
+def test_collide_wall_w(game):
+    player = game.player #should be brand new
+    movement = pygame.event.Event(pygame.KEYDOWN,dict(key=pygame.K_w))
+    game.handle_event(movement)
+    
+    for i in range(20):
+        movement = pygame.event.Event(pygame.KEYDOWN,dict(key=pygame.K_a))
+        game.handle_event(movement)
+    x = player.get(Position).x
+    y = player.get(Position).y
+    print(player.get(Position))
+    assert (x,y) == (1,8)
