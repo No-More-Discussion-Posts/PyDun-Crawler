e12dfd76f7b1934f6375439dbc6b03a6e4281dd7
jfergesonumgc <150169966+jfergesonumgc@users.noreply.github.com>
2024-07-05 14:20:54 -0400

    Start of pytmx loading

--- app/db_demo.py
+++ app/db_demo.py
@@ -3,17 +3,18 @@
 
 """
 
-import pygame
-import os
-import sys
+import pygame, os, sys
+from pytmx.util_pygame import load_pygame
 from api import DungeonDB
 
-if "copd" not in sys.modules:
-    parent_dir_name = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-    print(f"Adding: {parent_dir_name}")
-    sys.path.append(parent_dir_name)
-from copd.engine import Engine
-from copd.ui.tiles import Map
+
+
+# if "copd" not in sys.modules:
+#     parent_dir_name = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+#     print(f"Adding: {parent_dir_name}")
+#     sys.path.append(parent_dir_name)
+# from copd.engine import Engine
+# from copd.ui.tiles import Map
 
 MAP_X = 40
 MAP_Y = 20
@@ -24,7 +25,7 @@ def create_demo_map(x, y):
     return map
         
 def main() -> None:
-    """Main game loop"""
+    """Main "game" loop"""
     '''
     game = Engine()
     #game.load_start_map((0, 0, 255), map=DEFAULT_MAP)
@@ -38,11 +39,49 @@ def main() -> None:
     game.Test_Grid()
     '''
 
+    # using https://www.youtube.com/watch?v=N6xqCwblyiw as a guide
+
+    ### Starting over with a fresh pygame window while testing pytmx ###
+    pygame.init()
+    screen = pygame.display.set_mode((MAP_X*16,MAP_Y*16))
+    tmx_data = load_pygame('menu.tmx')
+    sprite_group = pygame.sprite.Group()
+
+    menu_items = ('COPD: DB DEMO', 'BROWSE TABLES', 'NEW MAP')
+
+    for layer in tmx_data.layers:
+        if layer.name in menu_items:
+
+        # if hasattr(layer, 'data'):
+             for x, y, surf in layer.tiles():
+                 #print(dir(layer))
+                 pos = (x * 16, y * 16)
+                 Tile(pos = pos, surf = surf, groups = sprite_group)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            
+        screen.fill('black')
+        sprite_group.draw(screen)
+        pygame.display.update()
+
+
+
+
     # game.run()
-    db = DungeonDB()
-    map = db.generate_new_level()
-    print(map)
-    
+    # db = DungeonDB()
+    # map = db.generate_new_level()
+    # print(map)
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, pos, surf, groups):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_rect(topleft = pos)
+
 
 class demo_engine:
     #TODO hook this into the ecs and create a simple map engine
